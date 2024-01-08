849798dd2913f290593669b5819d8ccbea9d3bc1
“Muttenl” <fujingkai92@gmail.com>
2024-01-09 05:11:17 +0800

    Site updated: 2024-01-09 05:11:17

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/01/07/hello-world/"];function toRandomPost(){
+var posts=["2024/01/08/博客搭建/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
